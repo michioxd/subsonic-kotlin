99c66597a9a944069354c5d59bec5cc00f52028c
zt64 <31907977+zt64@users.noreply.github.com>
2026-03-11 19:44:54 -0400

    fix: add serial names for Starred response class

--- subsonic-api/src/commonMain/kotlin/dev/zt64/subsonic/api/model/Starred.kt
+++ subsonic-api/src/commonMain/kotlin/dev/zt64/subsonic/api/model/Starred.kt
@@ -1,5 +1,6 @@
 package dev.zt64.subsonic.api.model
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 /**
@@ -11,7 +12,10 @@ import kotlinx.serialization.Serializable
  */
 @Serializable
 public data class Starred internal constructor(
+    @SerialName("artist")
     val artists: List<Artist> = emptyList(),
+    @SerialName("album")
     val albums: List<Album> = emptyList(),
+    @SerialName("song")
     val songs: List<Song> = emptyList()
 )
