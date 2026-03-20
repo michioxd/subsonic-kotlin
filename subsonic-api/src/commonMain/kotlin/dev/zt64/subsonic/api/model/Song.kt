2a16c07fc2699e723b56773bddd29d0e1d1948f8
zt64 <31907977+zt64@users.noreply.github.com>
2026-03-19 22:40:52 -0400

    feat: add extra fields to song model

--- subsonic-api/src/commonMain/kotlin/dev/zt64/subsonic/api/model/Song.kt
+++ subsonic-api/src/commonMain/kotlin/dev/zt64/subsonic/api/model/Song.kt
@@ -3,6 +3,9 @@ package dev.zt64.subsonic.api.model
 import dev.zt64.subsonic.api.model.serializer.SubsonicDurationSerializer
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.builtins.ListSerializer
+import kotlinx.serialization.builtins.serializer
+import kotlinx.serialization.json.*
 import kotlin.time.Duration
 import kotlin.time.Instant
 
@@ -16,11 +19,17 @@ import kotlin.time.Instant
  * @property albumTitle Name of the album
  * @property albumId Unique identifier for the album
  * @property parentId ID of the parent directory
+ * @property comment The comment tag of the song
  * @property trackNumber Track number on the album
  * @property discNumber Disc number for multi-disc albums
+ * @property isrc List of ISRC(s)
  * @property year Release year
  * @property genre Music genre
+ * @property genres List of all genres of the song
+ * @property moods List of moods, or empty if none
  * @property duration Length of the song
+ * @property bpm Beats per minute of the song
+ * @property contributors List of people who contributed to the song (e.g., composer, performer)
  * @property playCount Number of times the song has been played
  * @property userRating User's rating (1-5)
  * @property averageRating Average rating across all users
@@ -28,6 +37,8 @@ import kotlin.time.Instant
  * @property bitDepth Audio bit depth
  * @property sampleRate Audio sample rate in hertz
  * @property audioChannelCount Number of audio channels
+ * @property replayGain Replay gain info
+ * @property explicitStatus Explicit content status of the song
  * @property fileSize Size of the file in bytes
  * @property fileExtension File extension (e.g., mp3, flac)
  * @property mimeType MIME type of the file
@@ -47,13 +58,20 @@ public data class Song internal constructor(
     val albumId: String? = null,
     @SerialName("parent")
     val parentId: String? = null,
+    val comment: String? = null,
     @SerialName("track")
     val trackNumber: Int? = null,
     val discNumber: Int? = null,
+    val isrc: List<String> = emptyList(),
     val year: Int? = null,
     val genre: String? = null,
+    @Serializable(GenresSerializer::class)
+    val genres: List<String> = emptyList(),
+    val moods: List<String> = emptyList(),
     @Serializable(SubsonicDurationSerializer::class)
     val duration: Duration,
+    val bpm: Int? = null,
+    val contributors: List<Contributor> = emptyList(),
     val playCount: Int = 0,
     val userRating: Int? = null,
     val averageRating: Int? = null,
@@ -64,6 +82,8 @@ public data class Song internal constructor(
     val sampleRate: Int,
     @SerialName("channelCount")
     val audioChannelCount: Int,
+    val replayGain: ReplayGain? = null,
+    val explicitStatus: ExplicitStatus? = null,
     @SerialName("size")
     val fileSize: Long,
     @SerialName("suffix")
@@ -88,6 +108,50 @@ public data class Song internal constructor(
         /** Video file */
         VIDEO
     }
+
+    /**
+     * Replay gain data of a song
+     *
+     * @property albumGain Album gain value in dB
+     * @property albumPeak Album peak value
+     * @property trackGain Track gain value in dB
+     * @property trackPeak Track peak value
+     * @property baseGain Base gain value in dB
+     * @property fallbackGain Optional fallback gain in dB clients should apply when the corresponding
+     * gain value is missing.
+     */
+    @Serializable
+    public data class ReplayGain(
+        val albumGain: Float? = null,
+        val albumPeak: Float? = null,
+        val trackGain: Float? = null,
+        val trackPeak: Float? = null,
+        val baseGain: Float? = null,
+        val fallbackGain: Float? = null
+    )
+
+    /**
+     * Explicit status
+     */
+    @Serializable
+    public enum class ExplicitStatus {
+        @SerialName("explicit")
+        EXPLICIT,
+
+        @SerialName("clean")
+        CLEAN,
+
+        @SerialName("")
+        UNKNOWN
+    }
+
+    private class GenresSerializer : JsonTransformingSerializer<List<String>>(
+        tSerializer = ListSerializer(String.serializer())
+    ) {
+        override fun transformDeserialize(element: JsonElement): JsonElement {
+            return JsonArray(element.jsonArray.map { it.jsonObject["name"]!! })
+        }
+    }
 }
 
 /**
@@ -99,7 +163,8 @@ public data class Song internal constructor(
 @Serializable
 public data class Contributor internal constructor(
     val role: String,
-    val subRole: String
+    val subRole: String? = null,
+    val artist: Artist
 )
 
 /**
